dc7380d25700df720a7f63f39ad0ecd38b674e70
qipai <qipai12345@163.com>
2019-06-07 16:09:55 +0800

    修改fsm.CurState为公开字段

--- fsm/fsm.go
+++ fsm/fsm.go
@@ -5,14 +5,14 @@ type ActionType int
 type handler func(action ActionType, args ...interface{}) (nextState StateType)
 
 type FSM struct {
-	curState    StateType             // 当前状态
+	CurState    StateType             // 当前状态
 	handlers    map[StateType]handler // 每个状态对应的函数
 	endHandlers map[StateType]bool    // 记录结束状态
 }
 
 func New(startState StateType) *FSM {
 	return &FSM{
-		curState:    startState,
+		CurState:    startState,
 		handlers:    make(map[StateType]handler),
 		endHandlers: make(map[StateType]bool),
 	}
@@ -22,19 +22,15 @@ func (me *FSM) AddState(state StateType, handler handler) {
 	me.handlers[state] = handler
 }
 
-// 直接设置状态机为指定状态，不触发回调函数
-func (me *FSM) SetState(stateType StateType) {
-	me.curState = stateType
-}
 
 func (me *FSM) Do(action ActionType, args ...interface{}) (finished bool) {
-	h, ok := me.handlers[me.curState]
+	h, ok := me.handlers[me.CurState]
 	if !ok {
 		return
 	}
-	finished, _ = me.endHandlers[me.curState]
+	finished, _ = me.endHandlers[me.CurState]
 	if s:=  h(action, args...);s!=0{
-		me.curState = s
+		me.CurState = s
 	}
 	return
 }
