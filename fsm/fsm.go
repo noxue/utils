184a0be11ce032c0baa8e6879aa16c66615dc0ec
qipai <qipai12345@163.com>
2019-06-07 16:16:26 +0800

    修改fsm.CurState为公开字段

--- fsm/fsm.go
+++ fsm/fsm.go
@@ -2,23 +2,23 @@ package fsm
 
 type StateType int
 type ActionType int
-type handler func(action ActionType, args ...interface{}) (nextState StateType)
+type Handler func(action ActionType, args ...interface{}) (nextState StateType)
 
 type FSM struct {
 	CurState    StateType             // 当前状态
-	handlers    map[StateType]handler // 每个状态对应的函数
+	handlers    map[StateType]Handler // 每个状态对应的函数
 	endHandlers map[StateType]bool    // 记录结束状态
 }
 
 func New(startState StateType) *FSM {
 	return &FSM{
 		CurState:    startState,
-		handlers:    make(map[StateType]handler),
+		handlers:    make(map[StateType]Handler),
 		endHandlers: make(map[StateType]bool),
 	}
 }
 
-func (me *FSM) AddState(state StateType, handler handler) {
+func (me *FSM) AddState(state StateType, handler Handler) {
 	me.handlers[state] = handler
 }
 
